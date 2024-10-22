162fe6087b07fccbefa31e87acdc986e5802b9f8
showrav dhar <dhardip349900@gmail.com>
2024-10-22 18:53:44 +0600

    Learning Json

---
+++ YoutubeManagerApp/PreRequisite.py
@@ -0,0 +1,16 @@
+# my_li = ['apple', 'banana', 'strawberry']
+#
+# for key, value in enumerate(my_li):
+#     print(f"{key} - {value}")
+#
+# file = open('test1.py','w')
+
+# try: # we use try catch block to avoid error
+#     file.write('testing writing on file')
+# finally:
+#     file.close()
+
+# another way to access and close file
+
+# with open('YoutubeManagerApp.py','w') as file:
+#     file.write('Testing')
