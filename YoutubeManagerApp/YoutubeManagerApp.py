d0667e7440712b5c325bf6b57ee9dbcfc0e0aa8e
showrav dhar <dhardip349900@gmail.com>
2024-10-23 01:33:54 +0600

    Project done

--- YoutubeManagerApp/YoutubeManagerApp.py
+++ YoutubeManagerApp/YoutubeManagerApp.py
@@ -5,37 +5,66 @@ def load_data():
     try:
         with open('youtube.txt', 'r') as file:
             return json.load(file)
-    except:
-        pass
+    except FileNotFoundError:
+        return []
+
+
+def save_data_helper(videos):
+    with open('youtube.txt', 'w') as file:
+        json.dump(videos, file)
 
 
 def list_all_videos(videos):
-    pass
+    print("\n")
+    print('~' * 50)
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}. name = {video['name']} | Duration = {video['time']}")
+    print("\n")
+    print('~' * 50)
 
 
 def add_video(videos):
-    pass
+    name = input("Enter video name - ")
+    time = input("Enter video time - ")
+    videos.append({'name': name, 'time': time})
+    save_data_helper(videos)
 
 
 def update_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter video number to update - "))
+    if 1 <= index <= len(videos):
+        name = input("Enter the new video name - ")
+        length = input("Enter the new video time - ")
+        videos[index - 1] = {'name': name, 'time': length}
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected")
 
 
 def delete_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int (input("Enter the video number to be deleted - "))
+
+    if 1 <= index <= len(videos):
+        del videos[index - 1]
+        save_data_helper(videos)
+    else:
+        print("Invalid video index selected")
 
 
 def main():  # program starts from here
     videos = load_data()
     while True:
         print("\nYoutube Manager || Choose an option")
-        print("1. List all favourite videos")
+        print("1. List all your youtube videos")
         print("2. Add a youtube video")
         print("3. Update a youtube video details")
         print("4. Delete a youtube video")
-        print("4. Exit the app")
+        print("5. Exit the app")
 
-        choice = input("Enter Your Option")
+        choice = input("Enter Your Option - ")
+        # print(videos)
 
         match choice:
             case '1':
